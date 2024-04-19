6bab90b8636edbcc07754d0048f9bbfdf66ac3c8
ethan <ethan@kentik.com>
2024-04-19 14:45:11 -0700

    remove custom dimension work

--- CustomDimenstions/elastisearchIndexToKentik/modules/outputs.py
+++
@@ -1,6 +0,0 @@
-# MODULE FOR CREATING PRINT STRUCTURES FOR THE API DATA, TO ADD DIFFERENT PRINTING STRUCTURES, ADD ADDITIONAL FUNCTIONS.
-def printStructured(response_dict):
-    print(response_dict)
-
-def uploadToKentik(custom_dimensions):
-    print(custom_dimensions)
